450b4c4453c6ec15277410ac20a434db57932bc3
adyson <adyv8@yahoo.com.br>
2023-09-09 10:29:11 -0300

    2º commit - tela inicial finalizada

--- app/src/main/java/com/example/noproblem/MainActivity.kt
+++ app/src/main/java/com/example/noproblem/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.noproblem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnTelaInicial = findViewById<Button>(R.id.btnTelaInicial)
+
+        btnTelaInicial.setOnClickListener {
+            val intent = Intent(this,PerguntaUmActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/noproblem/PerguntaUmActivity.kt
+++ app/src/main/java/com/example/noproblem/PerguntaUmActivity.kt
@@ -2,10 +2,21 @@ package com.example.noproblem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class PerguntaUmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pergunta_um)
+        val txtPerguntaUm = findViewById<EditText>(R.id.txtPerguntaUm)
+        val btnTelaPerguntaUm = findViewById<Button>(R.id.btnTelaPerguntaUm)
+
+        btnTelaPerguntaUm.setOnClickListener {
+            val texto = txtPerguntaUm.text.toString()
+            Toast.makeText(this,texto,Toast.LENGTH_SHORT).show()
+        }
+
     }
 }

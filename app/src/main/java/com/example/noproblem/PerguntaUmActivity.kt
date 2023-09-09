ab2a794e26d4d23b6689ad638acb5e06d3230a2a
adyson <adyv8@yahoo.com.br>
2023-09-09 10:23:52 -0300

    2° commit - tela inicial finalizada

---
+++ app/src/main/java/com/example/noproblem/PerguntaUmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.noproblem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerguntaUmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pergunta_um)
+    }
+}
